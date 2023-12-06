ce4236009e0cee1888275df901caf93273a2b1a8
Emanuel Gomez <emagozz@gmail.com>
2023-12-06 13:58:11 -0300

    Finish movement in game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
   const createEmptyMatrix = (rows: number, columns: number) => {
@@ -9,71 +9,103 @@ export default function Home() {
   }
 
   const [matrix, setMatrix] = useState<number[][]>(createEmptyMatrix(18, 10))
+  const [piecePosition, setPiecePosition] = useState({ x: 0, y: 4 })
+  const [piece, setPiece] = useState<number[][]>([
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ])
 
-  const placePiece = (piece: number[][], x: number, y: number) => {
-    setMatrix((prevMatrix) => {
-      const newMatrix = prevMatrix.map((row) => [...row])
-      //Iterate over the piece and place it in the matrix
-      piece.forEach((row, rowIndex) => {
-        row.forEach((value, cellIndex) => {
-          if (value) {
-            newMatrix[rowIndex + x][cellIndex + y] = value
-          }
-        })
-      })
-      return newMatrix
-    })
-  }
+  const placePieceOnBoard = useCallback(
+    (piece: number[][]) => {
+      setMatrix((prevMatrix) => {
+        const newBoard = prevMatrix.map((row) => [...row])
 
-  useEffect(() => {
-    //For now I added this generic piece but I need the generic dynamically
-    placePiece(
-      [
-        [1, 1],
-        [1, 1],
-      ],
-      0,
-      4
-    )
-
-    placePiece(
-      [
-        [1, 1],
-        [1, 1],
-      ],
-      16,
-      4
-    )
-  }, [])
-
-  useEffect(() => {
-    //The idea is remove useEffect and use keyDown but I don't know why is not working
-    const handleKeyDown = (event: any) => {
-      const movePieceBottom = () => {
-        setMatrix((prevMatrix) => {
-          const newMatrix = prevMatrix.map((rowArr) => rowArr.slice())
-          const matrixLength = newMatrix.length
-
-          for (let x = matrixLength - 1; x >= 0; x--) {
-            for (let y = 0; y < newMatrix[x].length; y++) {
-              //If the current cell is not empty
-              if (newMatrix[x][y] !== 0) {
-                //If the next cell is not empty  and I'm not in the bottom of the tetris
-                if (x + 1 < matrixLength && newMatrix[x + 1][y] === 0) {
-                  //Move the current cell to the next cell
-                  newMatrix[x + 1][y] = newMatrix[x][y]
-                  // Set the current cell to empty
-                  newMatrix[x][y] = 0
-                }
-              }
+        for (let row = 0; row < piece.length; row++) {
+          for (let column = 0; column < piece[row].length; column++) {
+            if (piece[row][column] !== 0) {
+              newBoard[row + piecePosition.x][column + piecePosition.y] =
+                piece[row][column]
             }
           }
-          return newMatrix
-        })
+        }
+
+        return newBoard
+      })
+    },
+    [piecePosition]
+  )
+
+  const movePieceOnBoard = useCallback(
+    (
+      board: number[][],
+      piece: number[][],
+      position: { x: number; y: number }
+    ) => {
+      // Create a new board based on the previous state
+      const newBoard = board.map((row) => [...row])
+
+      // Clear the previous position of the piece
+      for (let row = 0; row < piece.length; row++) {
+        for (let column = 0; column < piece[row].length; column++) {
+          if (piece[row][column] !== 0) {
+            newBoard[row + piecePosition.x][column + piecePosition.y] = 0
+          }
+        }
       }
 
+      // Place the piece in the new position
+      for (let row = 0; row < piece.length; row++) {
+        for (let column = 0; column < piece[row].length; column++) {
+          if (piece[row][column] !== 0) {
+            newBoard[row + position.x][column + position.y] = piece[row][column]
+          }
+        }
+      }
+
+      setMatrix(newBoard)
+    },
+    [piecePosition]
+  )
+
+  useEffect(() => {
+    placePieceOnBoard(piece)
+    // placePieceOnBoard(matrix, piece, { x: 15, y: 4 })
+  }, [piece, placePieceOnBoard])
+
+  const movePiece = useCallback(
+    (direction: 'left' | 'right' | 'down') => {
+      let newX = piecePosition.x
+      let newY = piecePosition.y
+
+      switch (direction) {
+        case 'left':
+          newY -= 1
+          break
+        case 'right':
+          newY += 1
+          break
+        case 'down':
+          newX += 1
+          break
+        default:
+          break
+      }
+      movePieceOnBoard(matrix, piece, { x: newX, y: newY })
+      setPiecePosition({ x: newX, y: newY })
+    },
+    [piecePosition.x, piecePosition.y, movePieceOnBoard, matrix, piece]
+  )
+
+  //Detect when move a the current piece
+  useEffect(() => {
+    const handleKeyDown = (event: any) => {
       if (event.key === 'ArrowDown') {
-        movePieceBottom()
+        movePiece('down')
+      } else if (event.key === 'ArrowLeft') {
+        movePiece('left')
+      } else if (event.key === 'ArrowRight') {
+        movePiece('right')
       }
     }
 
@@ -82,7 +114,7 @@ export default function Home() {
     return () => {
       document.removeEventListener('keydown', handleKeyDown)
     }
-  }, [])
+  }, [movePiece, piece, piecePosition])
 
   return (
     <div>
